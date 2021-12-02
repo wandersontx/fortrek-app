3775b4edc72638ce59e8fa9b1908fa87fb598377
Wanderson Teixeira <wandersonh89@gmail.com>
2021-12-01 23:42:09 -0300

    ajuste campo fundador

--- src/app/shared/marca.model.ts
+++ src/app/shared/marca.model.ts
@@ -3,7 +3,7 @@ export class Marca {
   public marcaId: number = 0;
   public nome: string = '';
   public ano_fundacao: string = '';
-  public fundador: string = '';
+  public fundadores: string = '';
   public sede: string = '';
   public ceo: string = '';
   public site: string = '';
